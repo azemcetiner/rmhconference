42a6308c91e0f10913d484fab3a16d0a792c5da7
azemcetiner <azem_cetiner@hotmail.com>
2019-01-20 20:33:41 -0500

    added program page, added new speaker, added new logos, fixed styling

--- myscript.js
+++ myscript.js
@@ -1,5 +1,28 @@
 // Set the date we're counting down to
 var countDownDate = new Date("Jan 31, 2019 9:00:00").getTime();
+var slideIndex = 1;
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+setInterval(function(){currentSlide(slideIndex+1)}, 1500);
 
 // Update the count down every 1 second
 var x = setInterval(function() {
